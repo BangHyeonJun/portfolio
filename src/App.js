79e46a74809ff200a61ffb7f2e51552bc617bbb0
BangHyeonJun <bhj2849@naver.com>
2018-08-02 14:40:48 +0900

    Making NavBar and Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
-import test11 from "./components/test";
+import TemporaryDrawer from "./components/TemporaryDrawer";
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <test11 />
+                <NavBar />
             </div>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import MenuList from "./MenuList";
 
 const NavBar = () => {
-    const TempPropTypes = PropTypes.object.isRequired;
     return (
         <div>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        className={TempPropTypes.menuButton}
-                        color="inherit"
-                        aria-label="Menu"
-                    >
-                        <MenuIcon />
-                    </IconButton>
+                    <MenuList />
                     <Typography variant="title" color="inherit">
                         UmNyaUm's Portfolio Blog
                     </Typography>
